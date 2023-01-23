885f070213d1ca30780442a06cc31e556803af67
RatnadipBharde <ratnadipbharde@gmail.com>
2023-01-24 02:34:51 +0530

    added code for reset employee payroll form

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -31,6 +31,35 @@ const save = () => {
     }
 }
 
+const resetForm = () => {
+    setValue('#name', '');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary', '');
+    setValue('#notes', '');
+    setValue('#day', '1');
+    setValue('#month', 'January');
+    setValue('#year', '2020');
+}
+
+const unsetSelectedValues=(propertyValue)=>{
+let allItems=document.querySelectorAll(propertyValue);
+allItems.forEach(item=>{
+    item.checked=false;
+});
+}
+
+const setTextValue=(id,value)=>{
+    const element=document.querySelector(id);
+    element.textContent=value;
+}
+
+const setValue=(id,value)=>{
+    const element=document.querySelector(id);
+    element.value=value;
+}
+
 function createAndUpdateStorage(employeePayrollData){
     let employeePayrollList=JSON.parse(localStorage.getItem(employeePayrollData));
     if (employeePayrollList!=undefined) {
