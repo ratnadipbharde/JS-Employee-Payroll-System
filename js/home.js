6ff68a98e396541830301da6d5767e243cb3a8b8
RatnadipBharde <ratnadipbharde@gmail.com>
2023-01-24 20:56:21 +0530

    inject headerHtml code

--- js/home.js
+++ js/home.js
@@ -3,16 +3,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 const createInnerHtml = () => {
-    const innerHtml = `
-    <tr>
-    <th></th>
-    <th>Name</th>
-    <th>Gender</th>
-    <th>Department</th>
-    <th>Salary</th>
-    <th>Start Date</th>
-    <th>Action</th>
-</tr>
+    const headerHtml="<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Action</th></tr>"
+    const innerHtml = `${headerHtml}
 <tr>
     <td><img class="profile" alt=""
             src="/assets/profile-images/image1.jpg"></td>
